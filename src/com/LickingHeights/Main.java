147e8375103930277aa5a83f7e3afc0ace437652
Brandon Peach <brandon.peach.2022@student.lhschools.org>
2020-03-12 12:37:18 -0400

    Conditional implemented

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,31 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter either 1 or 2");
+        int number = keyboard.nextInt();
+
+        if(number == 1) {
+            System.out.println("The number is equal to 1");
+        }
+
+        else if(number == 2 ) {
+            System.out.println("The number is equal to 2");
+        }
+
+        else {
+            System.out.println("You did not follow directions");
+        }
+
+
+
+
+
     }
 }
